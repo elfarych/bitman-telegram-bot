1878fda328169ed3ad83b38626005fb3e807f93d
DESKTOP-CKK2L6K\Эльдар <elfarych@gmail.com>
2022-06-01 01:27:11 +0600

    1

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     binanceFuturesAPI: 'https://fapi.binance.com',
     binanceSpotAPI: 'https://api3.binance.com',
-    // backendURL: 'http://192.168.0.109:8800'
-    backendURL: 'http://192.168.0.155:8800'
+    backendURL: 'http://192.168.0.109:8800'
+    // backendURL: 'http://192.168.0.155:8800'
 }
